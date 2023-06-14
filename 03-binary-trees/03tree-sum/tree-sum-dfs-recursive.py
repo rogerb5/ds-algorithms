b4979d407e6ec2a8ee947dd7657acfe97dbdee13
rogerb5 <rbenjume@ewu.edu>
2023-06-13 21:41:41 -0700

    add new problem

---
+++ 03-binary-trees/03tree-sum/tree-sum-dfs-recursive.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, val):
+      self.val = val
+      self.left = None
+      self.right = None
+      
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       3
+#    /    \
+#   11     4
+#  / \      \
+# 4   -2     1
+
+# should result in 21
+
+
+# my solution recursive dfs
+def tree_sum(root):
