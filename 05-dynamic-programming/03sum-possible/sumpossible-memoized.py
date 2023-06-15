c389fdf7d5b9a0683bc444d3d5966b581185f4d3
rogerb5 <rbenjume@ewu.edu>
2023-06-15 13:08:08 -0700

    add notes and solution

---
+++ 05-dynamic-programming/03sum-possible/sumpossible-memoized.py
@@ -0,0 +1,23 @@
+# amount variable changes over time it get smaller and smaller
+def sum_possible(amount, numbers):
+  return _sum_possible(amount, numbers, {})
+
+def _sum_possible(amount, numbers, memo):
+  if amount in memo:
+    return memo[amount]
+  
+  if amount < 0:
+    return False
+  
+  if amount == 0:
+    return True
+  
+  for num in numbers:
+    if _sum_possible(amount - num, numbers, memo):
+      memo[amount] = True
+      return True
+    
+  memo[amount] = False  
+  return False
+
+sum_possible(8, [5, 12, 4]) # -> True, 4 + 4
---
+++ 05-dynamic-programming/03sum-possible/sumpossible-recursive.py
@@ -0,0 +1,16 @@
+def sum_possible(amount, numbers):
+    if amount < 0:
+     return False
+    
+    if amount == 0:
+     return True
+
+    for num in numbers:
+    # avoid infinite recursion
+     #if num <= amount
+         if sum_possible(amount - num, numbers) == True:
+             return True
+    
+    return False
+
+sum_possible(8, [5, 12, 4]) # True, 4 + 4
