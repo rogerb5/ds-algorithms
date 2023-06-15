9925f0b88ff33e3f8aa4ae0ce23ad32acd6c590b
rogerb5 <rbenjume@ewu.edu>
2023-06-15 11:49:46 -0700

    add notes and solutions

---
+++ 05-dynamic-programming/02tribonacci/trib-bruteforce-timeout.py
@@ -0,0 +1,10 @@
+def tribonacci(n):
+  if n == 0 or n == 1:
+    return 0
+
+  if n == 2:
+    return 1
+
+  return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
+
+tribonacci(1)
---
+++ 05-dynamic-programming/02tribonacci/trib-memoized.py
@@ -0,0 +1,22 @@
+def tribonacci(n):
+  return _tribonacci(n, {})
+# Our dictonary has keys that represent arguments to our recursive function
+# the associated values will represent the return value for that argument
+def _tribonacci(n, memo):
+
+  # if sub tree value has been encountered
+  if n in memo:
+    return memo[n]
+
+  if n == 0 or n == 1:
+    return 0
+
+  if n == 2:
+    return 1
+
+  # we will key into the memo using n!
+  memo[n] = _tribonacci(n - 1, memo) + _tribonacci(n - 2, memo) + _tribonacci(n - 3, memo)
+  print(memo[n])
+  return memo[n] #
+
+tribonacci(14)
