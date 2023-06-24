42190d113958b4f2332772b14ce862f5efd32003
rogerb5 <rbenjume@ewu.edu>
2023-06-24 11:28:02 -0700

    add notes and solution

--- 02-linked-list/06zipper-list/zipperlist-iterative.py
+++ 02-linked-list/06zipper-list/zipperlist-iterative.py
@@ -17,8 +17,26 @@ x.next = y
 y.next = z
 # x -> y -> z
 
+def zipper_lists(head_1, head_2):
+  tail = head_1
+  current_1 = head_1.next
+  current_2 = head_2
+  count = 0
+  while current_1 is not None and current_2 is not None:
+    if count % 2 == 0:
+      tail.next = current_2
+      current_2 = current_2.next
+    else:
+      tail.next = current_1
+      current_1 = current_1.next
+    tail = tail.next
+    count += 1
+    
+  if current_1 is not None:
+    tail.next = current_1
+  if current_2 is not None:
+    tail.next = current_2
 
+  return head_1
 
-
-# zipper_lists(a, x)
-# a -> x -> b -> y -> c -> z
+zipper_lists(a, x) # a -> x -> b -> y -> c -> z
