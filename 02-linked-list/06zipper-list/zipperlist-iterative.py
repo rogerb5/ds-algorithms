4c0101413a1652b538692cedd66977cf5f662df3
rogerb5 <rbenjume@ewu.edu>
2023-06-23 19:53:05 -0700

    add starter files

---
+++ 02-linked-list/06zipper-list/zipperlist-iterative.py
@@ -0,0 +1,24 @@
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+a.next = b
+b.next = c
+# a -> b -> c
+
+x = Node("x")
+y = Node("y")
+z = Node("z")
+x.next = y
+y.next = z
+# x -> y -> z
+
+
+
+
+# zipper_lists(a, x)
+# a -> x -> b -> y -> c -> z
